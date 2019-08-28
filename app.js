33de7e0622e2c2cb69ad2c681d2d8d524ad960b9
Abijith K <abijith973@gmail.com>
2019-08-28 15:35:10 +0530

    First push

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+console.log("Hello");
+
+// module.exports = app;
+
+exports.handler = function(event, context, callback) {
+	callback(null, "Hello World");
+}
